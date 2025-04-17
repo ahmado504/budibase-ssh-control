4d451cba77860656c3214ea0df7b1a80c5c7d79e
Ahmad Ottallah <ahmadottallah504@gmail.com>
2025-04-16 22:17:32 -0500

    chore: save work before pull --rebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // src/index.ts
-export {
-    startRecording,
-    stopRecording,
-    recordAndUpload
-  } from './automation';
-  
+import { startRecording, stopRecording } from './automation';
+/**
+ * Export both automations for Budibase (or whichever host) to pick up.
+ */
+export { startRecording, stopRecording };
+// Or, if your host expects an array of handlers:
+// export const automations = [ startRecording, stopRecording ];
