bd77833fd373d7f484c9c9ab33f6125cf9df2548
Ahmad Ottallah <ahmadottallah504@gmail.com>
2025-04-16 22:08:09 -0500

    chore: fix exports and align imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // src/index.ts
-import { startRecording, stopRecording } from './automation';
-/**
- * Export both automations for Budibase (or whichever host) to pick up.
- */
-export { startRecording, stopRecording };
-// Or, if your host expects an array of handlers:
-// export const automations = [ startRecording, stopRecording ];
+export {
+    startRecording,
+    stopRecording,
+    recordAndUpload
+  } from './automation';
+  
